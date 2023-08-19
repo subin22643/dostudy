a386051b8ec0c0e67b9aa972ecfbd2a566350345
subin22643 <bin577733@naver.com>
2023-08-19 19:37:35 +0900

    230819

---
+++ swea/1227 미로2.py
@@ -0,0 +1,32 @@
+def route(start):
+    visited = [[0] * 100 for _ in range(100)]
+    q = []
+    q.append(start)     # start = [1,1]
+    visited[start[0]][start[1]] = 1
+
+    while q:
+        w = q.pop(0)                # w = [1,1]
+        if maze[w[0]][w[1]] == 3:
+            return 1
+        for k in range(4):
+            ni = w[0]+di[k]
+            nj = w[1]+dj[k]
+            if 0 <= ni < 100 and 0 <= nj < 100 and maze[ni][nj] != 1 and visited[ni][nj] == 0:
+                q.append([ni, nj])
+                visited[ni][nj] = 1
+    return 0
+
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+for _ in range(10):
+    t = int(input())
+    maze = [list(map(int, input())) for __ in range(100)]
+
+    for i in range(100):
+        for j in range(100):
+            if maze[i][j] == 2:
+                start = [i, j]
+
+    result = route(start)
+    print(f'#{t}', result)
