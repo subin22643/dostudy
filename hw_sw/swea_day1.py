80f3d42679b739e1b1fcfcd43e121e8480587f1d
subin22643 <bin577733@naver.com>
2023-08-23 23:40:50 +0900

    230823

--- hw_sw/swea_day1.py
+++ hw_sw/swea_day1.py
@@ -1,23 +1,10 @@
-T = int(input())
-for t in (1, T+1):
-    N = int(input())
+for _ in range(1, 11):
+    T = int(input())
 
-    lst = [0] * 5000
-
-    for i in range(N):
-        a, b = map(int, input().split())
-
-        for j in range(a,b+1):
-            lst[j] += 1
-
-    P = int(input())
-    blank_list = []
-    for k in range(P):
-        a = int(input())
-        blank_list.append(lst[a])
-
-    print(f'#{t}', *blank_list)
-
+    lst = []
+    for _ in range(100):
+        lst.append(list(map(int, input().split())))
+    print(lst)
 
 
 
